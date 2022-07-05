a7b0f7e4b5c2a9165f6abd162da81f70f1972140
Maksymilian <ts3joker@gmail.com>
2022-07-05 09:38:56 +0200

    Initial Commit

---
+++ lesson6/lesson6App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson6App.swift
+//  lesson6
+//
+//  Created by Maksymilian Patoń on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct lesson6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
